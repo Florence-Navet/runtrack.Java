9f5cf3ac623fd0454d9ade76cb80806b84610383
AeRiaSaito <florence.navet@laplateforme.io>
2025-06-16 11:14:19 +0200

    feat : job2 doneÃ

---
+++ src/Prenom.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Prenom {
+    public static void AppelerPrenom() {
+        /*Écrire un programme java qui demande un prénom et qui répond « bonjour
+        prenom »*/
+        Scanner clavier = new Scanner(System.in);
+
+        // Demandez à l'utilisateur d'entrer des données.
+        System.out.print("Entrez votre prénom : ");
+        String prenom = clavier.nextLine();
+
+        // Affichez les données entrées par l'utilisateur.
+        System.out.println("Bonjour " + prenom);
+
+        // Fermez le scanner pour éviter les fuites de ressources.
+        clavier.close();
+
+    }
+}
